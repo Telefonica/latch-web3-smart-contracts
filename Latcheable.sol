ec78a701c73c118ddd374a85cb7753e6119e5b92
Davidcallegalindo <david.delacallegalindo@gmail.com>
2024-11-28 10:17:08 +0100

    Updata latcheable with isLatcheableMethod modifier

--- Latcheable.sol
+++ Latcheable.sol
@@ -1,21 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ILatchWeb3 {   
     function getStatus(address accountClient) external view returns (bool);
+    function getStatusMethod(address accountClient, string memory method) external view returns (bool);
 }
 
 abstract contract Latcheable is Ownable {
     address private PROXY_LATCH;
     
     function setProxyLatch(address proxyLatch) external onlyOwner {
+        require(proxyLatch != address(0x0), "Invalid address");
         PROXY_LATCH = proxyLatch;
     }
 
     modifier isLatcheable() {
         require(!ILatchWeb3(PROXY_LATCH).getStatus(msg.sender), "LatchClosed");
+        _;
+    }
+
+    modifier isLatcheableMethod(string memory method) {
+        require(!ILatchWeb3(PROXY_LATCH).getStatusMethod(msg.sender, method), "LatchClosed");
         _;
     }
 
